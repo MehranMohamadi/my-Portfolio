71212958d0bdb35a8f4c499960b52231aabf8186
Mehran <learnvueandreact@gmail.com>
2026-01-02 12:10:02 +0330

    chore : add seo parameters

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+ async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, follow",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,9 +1,56 @@
-// src/app/[locale]/layout.tsx
 import '../../styles/global.css';
 import { Providers } from '../../components/Provider';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import type { Metadata } from "next";
 
-export default async function LocaleLayout({ children, params }) {
+export const metadata: Metadata = {
+  keywords: [
+    "Frontend Developer",
+    "Nuxt",
+    "Vue",
+    "Next.js",
+    "Web Developer",
+    "JavaScript",
+  ],
+  title: {
+    default: "Mehrann Mohammadi | Frontend Developer | Virasty | Gap Massenger",
+    template: "%s | Mehrann Mohammadi",
+  },
+  description:
+    "Frontend developer specialized in Nuxt, Vue, and modern web technologies.",
+  alternates: {
+    canonical: "https://www.mehranmohammadifrd.ir",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  openGraph: {
+    title: "Mehrann Mohammadi | Frontend Developer",
+    description:
+      "Frontend developer specialized in Nuxt, Vue, and modern web technologies.",
+    url: "https://www.mehranmohammadifrd.ir",
+    siteName: "Mehrann Mohammadi",
+    locale: "en_US",
+    type: "website",
+    images: [
+      {
+        url: `https://www.mehranmohammadifrd.ir/og-image.jpg`,
+        width: 1200,
+        height: 630,
+        alt: "Mehrann Mohammadi - Frontend Developer",
+      },
+    ],
+  },
+};
+
+export default async function LocaleLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
   const { locale } = await params;
 
   let messages;
